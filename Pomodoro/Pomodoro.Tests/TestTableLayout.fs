d19023a1760cf40b9dfd40fc9b46fe4ded40f9dd
Stelios Georgiou <steliosdg@outlook.com>
2020-07-27 00:28:32 +0100

    refactored for a more functional style

--- Pomodoro/Pomodoro.Tests/TestTableLayout.fs
+++ Pomodoro/Pomodoro.Tests/TestTableLayout.fs
@@ -9,11 +9,8 @@ type TableLayoutForm() as this =
     inherit Form()
     
     do
-        this.ClientSize <- Size(500, 300)
-        this.Title <- "Table Layout"
-
-        let dropdown = new DropDown()
-
+        base.ClientSize <- Size(500, 300)
+        base.Title <- "Table Layout"
 
         // Timer controls
         // Might want to create command types for these
@@ -25,50 +22,25 @@ type TableLayoutForm() as this =
         let workInterval = new Slider()
         let shortBreak = new Slider()
         let longBreak = new Slider()
+        
+        let sliderLabels = 
+            [ "Set work interval"; "Set break interval"; "Set long break interval" ] 
+            |> List.map (fun comment -> new Label(Text = comment))
+            |> List.map (fun label -> label.TextAlignment <- TextAlignment.Center)
 
-        let lab1of1 = new Label(Text = "Set work interval")
-        let lab1of2 = new Label(Text = "Set break interval")
-        let lab1of3 = new Label(Text = "Set long break interval")
-        lab1of1.TextAlignment <- TextAlignment.Center
-        lab1of2.TextAlignment <- TextAlignment.Center
-        lab1of3.TextAlignment <- TextAlignment.Center
-
-        ["Item 1"; "Item 2"; "Item 3"]
-        |> List.iter dropdown.Items.Add
-
-        // The main layout mechanism for Eto.Forms is a TableLayout.
-        // This is recommended to allow controls to keep their natural platform-specific size.
-        // You can layout your controls declaratively using rows and columns as below, or add 
-        // to the TableLayout.Rows and TableRow.Cell directly.
         let layout = 
             new TableLayout(
                 Spacing = Size(5, 20) // space between each cell
                 , Padding = Padding(20)
             )
-
-        [
-            TableRow(
-                TableCell(playPause, true)
-                , TableCell(stop, true)
-                , TableCell(skip, true)
         
-            )
-            TableRow()
-            TableRow(
-                  TableCell(lab1of1)
-                , TableCell(lab1of2)
-                , TableCell(lab1of3)
-            )
-            // by default, the last row & column will get scaled. This adds a row at the end to 
-            // take the extra space of the form.
-            // otherwise, the above row will get scaled and stretch the TextBox/ComboBox/CheckBox
-            // to fill the remaining height.
-            TableRow(
-                TableCell(workInterval)
-                , TableCell(shortBreak)
-                , TableCell(longBreak)
-            )
-            TableRow(ScaleHeight = true)
+        //Table Structure
+        [
+            TableRow( [ playPause; stop; skip ] |> List.map TableCell )
+            TableRow( ScaleHeight = true )
+            TableRow( sliderLabels |> List.map TableCell )
+            TableRow( [ workInterval; shortBreak; longBreak ] |> List.map TableCell )
+            TableRow( ScaleHeight = true )
         ] |> List.iter layout.Rows.Add
 
         base.Content <- layout
