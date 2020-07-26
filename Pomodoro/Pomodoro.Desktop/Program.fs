b879690f2095894d58cf7a20f4ccf9d484c656d6
Stelios Georgiou <steliosdg@outlook.com>
2020-07-26 05:15:09 +0100

    Added project files

---
+++ Pomodoro/Pomodoro.Desktop/Program.fs
@@ -0,0 +1,12 @@
+﻿namespace Pomodoro.Desktop
+module Program =
+
+    open System
+    open Pomodoro
+
+    [<EntryPoint>]
+    [<STAThread>]
+    let Main(args) = 
+        let app = new Eto.Forms.Application(Eto.Platform.Detect)
+        app.Run(new MainForm())
+        0
